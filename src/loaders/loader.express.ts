fda604626c5bda213db5db8776c1d9b3bff5832b
new5558 <new17353@gmail.com>
2019-12-11 18:42:20 +0700

    fix: disable body parser warning

--- src/loaders/loader.express.ts
+++ src/loaders/loader.express.ts
@@ -16,7 +16,7 @@ declare global {
 export default ({ app }: any) => {
   app.use(cors());
   app.use(express.json());
-  app.use(express.urlencoded());
+  app.use(express.urlencoded({ extended: true }));
 
   app.use(routes);
 
